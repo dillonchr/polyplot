8d599b677d484893e002d9feb934e8cb80adb394
Dillon Christensen <j.dillon.christensen@gmail.com>
2017-05-12 14:08:56 -0500

    got search results to show up

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const currentLocation = require('./components/utils/current-location');
+let map;
 
 window['googleMapsOnReady'] = function () {
 
@@ -14,10 +15,25 @@ window['googleMapsOnReady'] = function () {
 
     currentLocation
         .then(coords => {
-            let map = app.createMap(coords);
+            map = app.createMap(coords);
             let marker = new google.maps.Marker({
                 position: coords,
                 map: map
             });
         });
+};
+
+window['searchPlaces'] = function (form) {
+    let service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        bounds: map.getBounds(),
+        keyword: form.querySelectorAll('.search__input').value
+    }, (results, status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            console.log(results);
+        } else {
+            console.error('failed', results, status);
+        }
+    });
+    return false;
 };
