3bb4ead896d81ce6cc6aae3133a7ad1212608869
Dillon Christensen <j.dillon.christensen+git@gmail.com>
2017-05-13 00:33:15 -0500

    updated karma config to allow coverage reporting!

--- karma.conf.js
+++ karma.conf.js
@@ -4,26 +4,17 @@ const path = require('path');
 
 module.exports = function (config) {
     config.set({
-
         // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
-
-
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['jasmine'],
-
-
         // list of files / patterns to load in the browser
         files: [
             'tests/main.js'
         ],
-
-
         // list of files to exclude
         exclude: [],
-
-
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
@@ -33,51 +24,37 @@ module.exports = function (config) {
             module: {
                 rules: [
                     {
-                        test: /\.spec.js$/,
-                        include: path.resolve('./'),
+                        test: /\.js$/,
+                        exclude: /(node_modules|tests|\.spec\.js$)/,
                         loader: 'istanbul-instrumenter-loader'
                     }
                 ]
             }
         },
-
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['progress', 'coverage-istanbul'],
-        coverageInstanbulReporter: {
+        coverageIstanbulReporter: {
             reports: ['html'],
             dir: 'coverage',
             fixWebpackSourcePaths: true
         },
-
-
         // web server port
         port: 9876,
-
-
         // enable / disable colors in the output (reporters and logs)
         colors: true,
-
-
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_DEBUG,
-
-
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: false,
-
-
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: ['Chrome'],
-
-
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
         singleRun: false,
-
         // Concurrency level
         // how many browser should be started simultaneous
         concurrency: Infinity
